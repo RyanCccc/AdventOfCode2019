fb4784af0844c3ecb6dd659bb147fbb1a9d328a9
Rendong Chen <chenrd769@gmail.com>
2019-12-02 21:59:10 -0500

    Initial Commit

---
+++ AdventOfCode2019/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode2019
+//
+//  Created by Rendong Chen on 12/2/19.
+//  Copyright © 2019 Rendong Chen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
